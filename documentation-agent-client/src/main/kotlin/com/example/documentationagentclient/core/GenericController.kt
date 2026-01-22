596072a8636abc91431fe7ca02c60d8bde6362dd
Sergey Lelikov <ghubman1@gmail.com>
2026-01-22 21:37:13 +0700

    update application configurations and add Redis service with health checks

--- documentation-agent-client/src/main/kotlin/com/example/documentationagentclient/core/GenericController.kt
+++ documentation-agent-client/src/main/kotlin/com/example/documentationagentclient/core/GenericController.kt
@@ -61,8 +61,8 @@ class GenericController(
                     prompt,
                     OpenAiImageOptions.builder()
                         .quality("hd")
-                        .width(1024)
-                        .height(1024)
+                        .width(128)
+                        .height(128)
                         .N(1)
                         .build()
                 )
